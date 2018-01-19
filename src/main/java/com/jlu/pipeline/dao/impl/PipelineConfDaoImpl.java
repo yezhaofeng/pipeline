090c7661ddc2a16369a9f0aef9a798ddbb3a68d8
langshiquan <langshiquan@baidu.com>
2018-01-19 10:42:06 +0800

    rename

--- src/main/java/com/jlu/pipeline/dao/impl/PipelineConfDaoImpl.java
+++ src/main/java/com/jlu/pipeline/dao/impl/PipelineConfDaoImpl.java
@@ -3,12 +3,12 @@ package com.jlu.pipeline.dao.impl;
 import org.springframework.stereotype.Repository;
 
 import com.jlu.common.db.dao.AbstractBaseDao;
-import com.jlu.pipeline.dao.PipelineConfDao;
+import com.jlu.pipeline.dao.IPipelineConfDao;
 import com.jlu.pipeline.model.PipelineConf;
 
 /**
  * Created by langshiquan on 18/1/15.
  */
 @Repository
-public class PipelineConfDaoImpl extends AbstractBaseDao<PipelineConf> implements PipelineConfDao {
+public class PipelineConfDaoImpl extends AbstractBaseDao<PipelineConf> implements IPipelineConfDao {
 }
