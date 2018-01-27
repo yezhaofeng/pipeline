3ceae53915763fa57c0a5acc392ae7421b955da0
langshiquan <langshiquan@baidu.com>
2018-01-28 02:20:06 +0800

    todo

--- src/main/java/com/jlu/common/web/MonitorController.java
+++ src/main/java/com/jlu/common/web/MonitorController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MonitorController {
 
+    // TODO return String
     @RequestMapping("/monitor")
     public ResponseBean monitor() {
         return ResponseBean.TRUE;
