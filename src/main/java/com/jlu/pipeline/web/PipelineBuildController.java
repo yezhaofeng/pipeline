45c1a94c589407a62c9d43a26f065d8cffd4b39a
langshiquan <576506402@qq.com>
2018-01-30 23:56:39 +0800

    swagger-ui bugfix

--- src/main/java/com/jlu/pipeline/web/PipelineBuildController.java
+++ src/main/java/com/jlu/pipeline/web/PipelineBuildController.java
@@ -18,7 +18,6 @@ import com.wordnik.swagger.annotations.ApiOperation;
 /**
  * Created by langshiquan on 18/1/14.
  */
-@Api(value = "流水线构建")
 @RestController
 @RequestMapping("/pipeline/build")
 public class PipelineBuildController extends AbstractController {
@@ -26,8 +25,6 @@ public class PipelineBuildController extends AbstractController {
     @Autowired
     private IPipelineBuildService pipelineBuildService;
 
-    @ApiOperation(value = "根据流水线配置id构建流水线", httpMethod = "GET", response = ResponseBean.class, notes =
-            "使用triggerId可以使用具体的提交进行构建")
     @RequestMapping(value = "", method = RequestMethod.POST)
     public ResponseBean build(Long pipelineConfId, @RequestParam(required = false, defaultValue = "0") Long triggerId) {
         if (triggerId == 0L) {
@@ -38,7 +35,6 @@ public class PipelineBuildController extends AbstractController {
         return ResponseBean.TRUE;
     }
 
-    @ApiOperation(value = "根据配置id获取流水线构建列表")
     @RequestMapping(value = "", method = RequestMethod.GET)
     public List<PipelineBuildBean> getPipelineBuildBeans(Long pipelineConfId) {
         return pipelineBuildService.getPipelineBuildBean(pipelineConfId);
