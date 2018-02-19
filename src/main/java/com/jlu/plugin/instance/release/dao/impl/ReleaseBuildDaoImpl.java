3142890275cc55b0948af49c5b8ae932134204f8
576506402@qq.com <576506402@qq.com>
2018-02-19 15:16:13 +0800

    发版默认值

--- src/main/java/com/jlu/plugin/instance/release/dao/impl/ReleaseBuildDaoImpl.java
+++ src/main/java/com/jlu/plugin/instance/release/dao/impl/ReleaseBuildDaoImpl.java
@@ -1,6 +1,7 @@
 package com.jlu.plugin.instance.release.dao.impl;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
@@ -34,7 +35,8 @@ public class ReleaseBuildDaoImpl extends AbstractBaseDao<ReleaseBuild> implement
         ConditionAndSet conditionAndSet = new ConditionAndSet();
         conditionAndSet.put("module", module);
         conditionAndSet.put("status", PipelineJobStatus.SUCCESS);
-        List<ReleaseBuild> releaseBuilds = findHeadByProperties(conditionAndSet, null, 0, 1);
+        DescOrder descOrder = new DescOrder("id");
+        List<ReleaseBuild> releaseBuilds = findHeadByProperties(conditionAndSet, Arrays.asList(descOrder), 0, 1);
         return CollUtils.isEmpty(releaseBuilds) ? null : releaseBuilds.get(0);
     }
 }
